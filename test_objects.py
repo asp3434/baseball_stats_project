7e38416657e8f5d513627c848133a13c59be8856
Andrew Prescott <andrew.prescott.2019@gmail.com>
2023-01-31 19:38:31 -0700

    beginning of module for objects in OOP

---
+++ test_objects.py
@@ -0,0 +1,17 @@
+### A module for creating objects ###
+
+# Batter Object
+'''
+ - Right-handed vs left vs switch
+ - batting average
+ - on base percentage
+ 
+'''
+
+
+# Pitcher Object
+'''
+ - pitch arsenal
+ - strikeout percentage
+ - innings pitched
+'''
