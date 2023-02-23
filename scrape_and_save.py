c77931f43981a638d375fc64f4a776505fdcf87f
Andrew Prescott <andrew.prescott.2019@gmail.com>
2023-02-23 16:54:43 -0700

    More notes for web scraping

--- scrape_and_save.py
+++ scrape_and_save.py
@@ -6,20 +6,38 @@ https://www.baseball-reference.com/leagues/majors/2019.shtml
 https://www.baseball-reference.com/leagues/majors/2021.shtml
 https://www.baseball-reference.com/leagues/majors/2022.shtml
 
+Steps:
  - Grab link on those pages to the World Series winner
- - crawl batting table to go through links for each player
+ - crawl batting table to access links for each player
  - grab link to gamelogs for that year (in a dropdown tab)
- - scrape the table
+ - scrape the table of all games that year
 
  - do the same for pitchers
+ 
+NOTE: There is a button to export the table as a .csv. Would it be easier to select that? Is there a way to do that?
+
 '''
 '''
 MVP Web Scraping
 
+https://www.baseball-reference.com/awards/mvp.shtml
 
+Steps: 
+ - crawl through table to access player links
+ - grab link to gamelogs for that year (in a dropdown tab)
+ - scrape the table of all games that year
+ 
+NOTE: There is a button to export the table as a .csv. Would it be easier to select that? Is there a way to do that?
 '''
 '''
 Cy Young Web Scraping
 
+https://www.baseball-reference.com/awards/cya.shtml
 
+Steps: 
+ - crawl through table to access player links
+ - grab link to gamelogs for that year (in a dropdown tab)
+ - scrape the table of all games that year
+ 
+NOTE: There is a button to export the table as a .csv. Would it be easier to select that? Is there a way to do that?
 '''
