ca4590bbc090d34c7d6eb4c262002367e68b6123
Andrew Prescott <andrew.prescott.2019@gmail.com>
2023-02-23 16:45:17 -0700

    Notes for web scraping

---
+++ scrape_and_save.py
@@ -0,0 +1,25 @@
+'''
+World Series Web Scraping
+
+loop through these websites:
+https://www.baseball-reference.com/leagues/majors/2019.shtml
+https://www.baseball-reference.com/leagues/majors/2021.shtml
+https://www.baseball-reference.com/leagues/majors/2022.shtml
+
+ - Grab link on those pages to the World Series winner
+ - crawl batting table to go through links for each player
+ - grab link to gamelogs for that year (in a dropdown tab)
+ - scrape the table
+
+ - do the same for pitchers
+'''
+'''
+MVP Web Scraping
+
+
+'''
+'''
+Cy Young Web Scraping
+
+
+'''
